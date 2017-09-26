cc619e1b6c2d7f390303f524a2b17b58588205bc
Inna Shpota <shpota_inna@ukr.net>
2017-09-26 12:43:23 +0300

    Added Auotowired in service

--- docker/create_tables.sql
+++ docker/create_tables.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    lastName TEXT NOT NULL,
-    firstName TEXT NOT NULL,
-    middleName TEXT NOT NULL
+    lastName VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    middleName VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users (lastName, firstName, middleName) VALUES
--- src/main/java/com/shpota/users/UsersService.java
+++ src/main/java/com/shpota/users/UsersService.java
@@ -1,34 +1,27 @@
 package com.shpota.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class UsersService {
-    private Map<Integer, User> users = new HashMap<>();
 
-    {
-        users.put(1, new User("Kovalev", "A.", "A."));
-        users.put(2, new User("Zebrov", "B.", "K."));
-        users.put(3, new User("Antonov", "D.", "I."));
-    }
-
-    public void createUser(User user) {
-        users.put(user.getId(), user);
-    }
-
-    public User getUser(int id) {
-        return users.get(id);
-    }
-
-    public void deleteUser(int id) {
-        users.remove(id);
-    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public Collection<User> getUsers() {
-        return users.values();
+        Collection<User> users = jdbcTemplate.query(
+                "SELECT * FROM users",
+                (rs, rowNum) -> new User(
+                        rs.getInt("id"),
+                        rs.getString("lastName"),
+                        rs.getString("firstName"),
+                        rs.getString("middleName")
+                )
+        );
+        return users;
     }
 }
