9a6fdaf4949d17a4dc54c90ff6de44f7757bb5d6
Inna Shpota <shpota_inna@ukr.net>
2017-09-27 18:45:02 +0300

    Added delete User

--- src/main/java/com/shpota/users/pages/HomePage.java
+++ src/main/java/com/shpota/users/pages/HomePage.java
@@ -6,8 +6,8 @@ import com.shpota.users.UsersService;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -33,9 +33,19 @@ public class HomePage extends WebPage {
         return new ListView<User>("users", users) {
             public void populateItem(final ListItem<User> item) {
                 final User user = item.getModelObject();
-                item.add(new Label("lastName", user.getLastName()));
-                item.add(new Label("firstName", user.getFirstName()));
-                item.add(new Label("middleName", user.getMiddleName()));
+                item.add(new Label("id", user.getId()));
+                item.add(new Label(
+                                "name",
+                                user.getLastName() + " " + user.getFirstName() + " " + user.getMiddleName()
+                        )
+                );
+                item.add(new Link<Void>("deleteLocationLink") {
+                    @Override
+                    public void onClick() {
+                        service.deleteUser(item.getModelObject().getId());
+                        setResponsePage(HomePage.class);
+                    }
+                });
             }
         };
     }
